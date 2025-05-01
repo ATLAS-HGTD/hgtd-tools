6a9fa5bbebacd3dcd5a640723477cc77c6128afe
Annika Stein <annika-stein@t-online.de>
2025-05-01 16:21:38 +0200

    feat: clickable slots and extras

--- data.py
+++ data.py
@@ -12,3 +12,168 @@ partIDs_for_Testing = {
     'Module' : 6573,
     #'Slot' : 
 }
+
+# define module geometry for canvas - not to scale!!!
+modLongSide = 80
+modShortSide = 40
+
+# define slots - not to scale!!!
+modR1M1 = {'slot':"R1M1", 'x':60, 'y':60, 'w':modLongSide, 'h':modShortSide}
+modR2M1 = {'slot':"R2M1", 'x':160, 'y':60, 'w':modLongSide, 'h':modShortSide}
+modR3M1 = {'slot':"R3M1", 'x':260, 'y':60, 'w':modLongSide, 'h':modShortSide}
+
+modR1M2 = {'slot':"R1M2", 'x':60, 'y':120, 'w':modLongSide, 'h':modShortSide}
+modR2M2 = {'slot':"R2M2", 'x':160, 'y':120, 'w':modLongSide, 'h':modShortSide}
+modR3M2 = {'slot':"R3M2", 'x':260, 'y':120, 'w':modLongSide, 'h':modShortSide}
+
+modR1M3 = {'slot':"R1M3", 'x':60, 'y':180, 'w':modLongSide, 'h':modShortSide}
+modR2M3 = {'slot':"R2M3", 'x':160, 'y':180, 'w':modLongSide, 'h':modShortSide}
+modR3M3 = {'slot':"R3M3", 'x':260, 'y':180, 'w':modLongSide, 'h':modShortSide}
+
+modR1M4 = {'slot':"R1M4", 'x':60, 'y':240, 'w':modLongSide, 'h':modShortSide}
+modR2M4 = {'slot':"R2M4", 'x':160, 'y':240, 'w':modLongSide, 'h':modShortSide}
+modR3M4 = {'slot':"R3M4", 'x':260, 'y':240, 'w':modLongSide, 'h':modShortSide}
+
+modR1M5 = {'slot':"R1M5", 'x':60, 'y':300, 'w':modLongSide, 'h':modShortSide}
+modR2M5 = {'slot':"R2M5", 'x':160, 'y':300, 'w':modLongSide, 'h':modShortSide}
+modR3M5 = {'slot':"R3M5", 'x':260, 'y':300, 'w':modLongSide, 'h':modShortSide}
+
+modR1M6 = {'slot':"R1M6", 'x':60, 'y':360, 'w':modLongSide, 'h':modShortSide}
+modR2M6 = {'slot':"R2M6", 'x':160, 'y':360, 'w':modLongSide, 'h':modShortSide}
+modR3M6 = {'slot':"R3M6", 'x':260, 'y':360, 'w':modLongSide, 'h':modShortSide}
+
+modR1M7 = {'slot':"R1M7", 'x':60, 'y':420, 'w':modLongSide, 'h':modShortSide}
+modR2M7 = {'slot':"R2M7", 'x':160, 'y':420, 'w':modLongSide, 'h':modShortSide}
+modR3M7 = {'slot':"R3M7", 'x':260, 'y':420, 'w':modLongSide, 'h':modShortSide}
+
+modROTATER2M1 = {'slot':"R2M1", 'x':160, 'y':120, 'w':modShortSide, 'h':modLongSide}
+modROTATER2M2 = {'slot':"R2M2", 'x':100, 'y':120, 'w':modShortSide, 'h':modLongSide}
+
+# define DUs - not to scale!!!
+allDUs = {
+    "BI01":[modR1M1, modR1M2, modR1M3,
+            modR2M1, modR2M2, modR2M3, modR2M4,
+            modR3M1, modR3M2, modR3M3, modR3M4],
+    "BI10":[modR1M1, modR1M2],
+    "BI12":[modR1M1, modR1M2, modR1M3, modR1M4,
+            modR2M1, modR2M2, modR2M3, modR2M4,
+            modR3M1, modR3M2, modR3M3, modR3M4],
+    "BM01":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5,
+            modR3M1, modR3M2, modR3M3, modR3M4],
+    "BM02":[modR1M1, modR1M2, modR1M3, modR1M4,
+            modR2M1, modR2M2, modR2M3, modR2M4,
+            modR3M1, modR3M2, modR3M3, modR3M4, modR3M5],
+    "BM03":[modR1M1, modR1M2, modR1M3,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5],
+    "BM04":[modR1M1, modR1M2, modR1M3, modR1M4,
+            modR2M1, modR2M2, modR2M3, modR2M4],
+    "BM05":[modR1M1, modR1M2, modR1M3,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5],
+    "BM06":[modR1M1, modR1M2],
+    "BM08":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5],
+    "BM09":[modR1M1, modR1M2, modR1M3,
+            modR2M1, modR2M2, modR2M3,
+            modR3M1, modR3M2],
+    "BM10":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5, modR1M6,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5, modR2M6],
+    "BM11":[modR1M1, modR1M2, modR1M3, modR1M4,
+            modR2M1, modR2M2, modR2M3, modR2M4,
+            modR3M1, modR3M2, modR3M3, modR3M4, modR3M5],
+    "BM12":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5,
+            modR3M1, modR3M2, modR3M3, modR3M4],
+    "BO01":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5,
+            modR3M1, modR3M2, modR3M3, modR3M4, modR3M5],
+    "BO02":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5, modR1M6,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5, modR2M6],
+    "BO03":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5, modR1M6,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5, modR2M6],
+    "BO04":[modR1M1, modR1M2,
+            modR2M1, modR2M2, modR2M3, modR2M4,
+            modR3M1, modR3M2, modR3M3, modR3M4],
+    "BO05":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5,
+            modR2M1, modR2M2, modR2M3, modR2M4,
+            modR3M1, modR3M2, modR3M3],
+    "BO06":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5, modR2M6],
+    "BO07":[modR1M1,
+            modR2M1, modR2M2, modR2M3],
+    "BO08":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5, modR1M6,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5, modR2M6],
+    "BO10":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5, modR1M6,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5, modR2M6],
+    "BO12":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5,
+            modR3M1, modR3M2, modR3M3, modR3M4, modR3M5, modR3M6],
+    "FI01":[modR1M1, modR1M2, modR1M3, modR1M4,
+            modR2M1, modR2M2, modR2M3, modR2M4,
+            modR3M1, modR3M2, modR3M3, modR3M4],
+    "FI10":[modR1M1,
+            modROTATER2M1, modROTATER2M2],
+    "FI12":[modR1M1, modR1M2, modR1M3, modR1M4,
+            modR2M1, modR2M2, modR2M3, modR2M4,
+            modR3M1, modR3M2, modR3M3, modR3M4],
+    "FM01":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5,
+            modR3M1, modR3M2, modR3M3, modR3M4, modR3M5],
+    "FM02":[modR1M1, modR1M2, modR1M3, modR1M4,
+            modR2M1, modR2M2, modR2M3, modR2M4,
+            modR3M1, modR3M2, modR3M3, modR3M4],
+    "FM03":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5,
+            modR2M1, modR2M2, modR2M3, modR2M4],
+    "FM04":[modR1M1, modR1M2, modR1M3, modR1M4,
+            modR2M1, modR2M2, modR2M3, modR2M4],
+    "FM05":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5, modR1M6,
+            modR2M1, modR2M2, modR2M3, modR2M4],
+    "FM06":[modR1M1, modR1M2],
+    "FM08":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5, modR2M6, modR2M7],
+    "FM09":[modR1M1, modR1M2, modR1M3, modR1M4,
+            modR2M1, modR2M2, modR2M3, modR2M4],
+    "FM10":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5],
+    "FM11":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5,
+            modR3M1, modR3M2, modR3M3, modR3M4],
+    "FM12":[modR1M1, modR1M2, modR1M3, modR1M4,
+            modR2M1, modR2M2, modR2M3, modR2M4,
+            modR3M1, modR3M2, modR3M3, modR3M4, modR3M5],
+    "FO01":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5,
+            modR3M1, modR3M2, modR3M3, modR3M4, modR3M5],
+    "FO02":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5, modR1M6,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5, modR2M6],
+    "FO03":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5, modR1M6,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5, modR2M6],
+    "FO04":[modR1M1, modR1M2, modR1M3, modR1M4,
+            modR2M1, modR2M2, modR2M3, modR2M4,
+            modR3M1, modR3M2, modR3M3],
+    "FO05":[modR1M1, modR1M2,
+            modR2M1, modR2M2, modR2M3, modR2M4,
+            modR3M1, modR3M2, modR3M3, modR3M4],
+    "FO06":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5, modR1M6,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5],
+    "FO07":[modR1M1, modR1M2, modR1M3, modR1M4,
+            modR2M1],
+    "FO08":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5],
+    "FO10":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5],
+    "FO12":[modR1M1, modR1M2, modR1M3, modR1M4, modR1M5,
+            modR2M1, modR2M2, modR2M3, modR2M4, modR2M5,
+            modR3M1, modR3M2, modR3M3, modR3M4, modR3M5],
+}
+
+totalNModules = 0
+allDUkeysStr = ''
+for key in allDUs.keys():
+    allDUkeysStr += f"{key},";
+    totalNModules += len(allDUs[key])
+
+fillColor_SU = '#f4f4bb'
+fillColor_Slot = '#ffffff'
+fillColor_SU_Text = '#aaaaaa'
+fillColor_ActiveSlot = '#33ff33'
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import customtkinter
+import threading
+import time
+import tkinter
+# If we need to tackle a longer list of options, there is a custom dropdown frame to handle them
+#from ctk_scrollDropdown import CTkScrollableDropdownFrame
+# Similar for overflowing text labels etc.
+#from ctk_scrollFrame import CTkXYFrame
 import api
 import data
 import util
-from pprint import pprint
-import time
-import threading
 
-customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_appearance_mode("Light")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 def load_relevant_parts(partKoP_shortname, onlyNonDeleted = True):
@@ -18,23 +22,50 @@ def load_relevant_parts(partKoP_shortname, onlyNonDeleted = True):
 def grab_relevant_SNs_and_partIDs(partsList):
     these_SNs_and_partIDs = util.getSortedListOfListsByNthColumn([[pL['serial_number'],pL['part_id']] for pL in partsList], 0)
     return these_SNs_and_partIDs
-    
+
+def isInSlot(rect, x, y):
+    isInSlot = False
+    left = rect['x']
+    right = rect['x']+rect['w']
+    top = rect['y']
+    bottom = rect['y']+rect['h']
+    if (right >= x
+        and left <= x
+        and bottom >= y
+        and top <= y):
+        isInSlot = True
+    return isInSlot
+
+'''
+// https://stackoverflow.com/a/5023867/22745629
+function getCursorPosition(canvas, event) {
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    console.log("x: " + x + " y: " + y);
+    return [x, y];
+}
+'''
+
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
         # configure window
         self.title("HGTD Tools")
-        self.geometry(f"{1500}x{900}")
+        self.geometry(f"{1500}x{1000}")
+        icon = tkinter.PhotoImage(file="windowIcon.png")
+        self.iconphoto(True,icon)
 
         # configure grid layout
         self.grid_columnconfigure((0, 1, 2), weight=1)
-        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure((0,1), weight=1)
 
         # create sidebar frame with widgets
-        self.sidebar_frame_left = customtkinter.CTkFrame(self, width=140, height=900, corner_radius=0)
+        self.sidebar_frame_left = customtkinter.CTkFrame(self, corner_radius=0)
         self.sidebar_frame_left.grid(row=0, column=0, rowspan=4, sticky="nsew")
-        self.sidebar_frame_left.grid_rowconfigure(4, weight=1, minsize=140)
+        self.sidebar_frame_left.grid_columnconfigure(0, weight=1)
+        self.sidebar_frame_left.grid_rowconfigure(4, weight=1)#, minsize=140)
         
         # fill sidebar
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame_left, text="HGTD Tools", font=customtkinter.CTkFont(size=20, weight="bold"))
@@ -42,31 +73,35 @@ class App(customtkinter.CTk):
 
         self.progress_label = customtkinter.CTkLabel(self.sidebar_frame_left, text="API Request Status")
         self.progress_label.grid(row=1, column=0, padx=20, pady=10)
-        self.progressbar = customtkinter.CTkProgressBar(self.sidebar_frame_left, orientation="horizontal")
+        self.progressbar = customtkinter.CTkProgressBar(self.sidebar_frame_left, orientation="horizontal", progress_color="#007711")
         self.progressbar.grid(row=2, column=0, padx=20, pady=10)
         self.progressbar.set(1)
 
         # work in main widget (column w.r.t. root >= 1)
-        self.explain_label = customtkinter.CTkLabel(self, text="Each Support Unit is oriented in such a way that when looking at its face, the module connectors are at the top (or on the right), and module capacitors are on the bottom (or on the left).\n\nUser actions (loading sites / assembly at CERN): First step at a loading site: fill the Detector Unit with modules, click on the canvas to select the correct position and use the button below. Once finished, move to the assembly step at CERN and enter the position manually when connecting a Detector Unit with the Detector (VxLxQx). Note: A back Detector Unit can only be on layer 1 or 2, a front Detector Unit can only be on layer 0 or 3.\n\n", font=customtkinter.CTkFont(size=16), height=200, wraplength=1000, justify="center")
-        self.explain_label.grid(row=0, column=1, padx=20, pady=(20, 0), sticky="nsew", columnspan=2)    
-
-        # create main frame with widgets
-        self.main_frame = customtkinter.CTkFrame(self, width=1200)
-        self.main_frame.grid(row=1, column=1, rowspan=1, columnspan=2, padx=20, pady=20, sticky="nsew")
+        self.explain_frame = customtkinter.CTkFrame(self)#CTkXYFrame(self)
+        self.explain_frame.grid(row=0, column=1, padx=20, pady=(20, 0), sticky="nsew", columnspan=2)
         
+        self.textbox = customtkinter.CTkTextbox(master=self.explain_frame, width=400, wrap='word')
+        self.textbox.pack(fill="both", expand=True, padx=20, pady=20)
+        #self.textbox.grid(row=0, column=0, sticky="nsew")
+        self.textbox.insert("0.0", "Each Support Unit is oriented in such a way that when looking at its face, the module connectors are at the top (or on the right), and module capacitors are on the bottom (or on the left).\n\nUser actions (loading sites / assembly at CERN): First step at a loading site: fill the Detector Unit with modules, click on the canvas to select the correct position and use the button below. Once finished, move to the assembly step at CERN and enter the position manually when connecting a Detector Unit with the Detector (VxLxQx). Note: A back Detector Unit can only be on layer 1 or 2, a front Detector Unit can only be on layer 0 or 3.")
+        self.textbox.configure(state='disabled')
+        # create main frame with widgets
+        self.main_frame = customtkinter.CTkFrame(self)
+        self.main_frame.grid(row=1, column=1, rowspan=1, columnspan=2, padx=20, pady=20, sticky="nsew")
         self.main_frame.grid_columnconfigure((0,1), weight=1)
         self.main_frame.grid_rowconfigure(1, weight=1)
 
         # button to select use case of the tool
-        self.segemented_button = customtkinter.CTkSegmentedButton(self.main_frame, values=["Module Loading", "Detector Assembly (CERN)"],
-                                                     command=self.segmented_button_callback,
-                                                     width=400, height=30)
-        self.segemented_button.set("Module Loading")
-        self.segemented_button.grid(row=0, column=0, padx=20, pady=(20, 0), sticky="nsew", columnspan=2)
+        self.segmented_button = customtkinter.CTkSegmentedButton(self.main_frame, values=["Module Loading", "Detector Assembly (CERN)"],
+                                                     command=self.segmented_button_callback)
+        self.segmented_button.set("Module Loading")
+        self.segmented_button.grid(row=0, column=0, padx=20, pady=(20, 0), sticky="nsew")
 
         # left sub widget: form
-        self.combobox_frame = customtkinter.CTkFrame(self.main_frame, width = 400)
+        self.combobox_frame = customtkinter.CTkFrame(self.main_frame, width = 600)
         self.combobox_frame.grid(row=1, column=0, padx=20, pady=20, sticky="nsew")
+        self.combobox_frame.grid_columnconfigure((0,1), weight=1)
 
         self.combobox_parent_T_label = customtkinter.CTkLabel(self.combobox_frame, text="Parent Part Type: Detector Unit")
         self.combobox_parent_T_label.grid(row=0, column=0, padx=20, pady=(10, 10), sticky="nsew") 
@@ -75,7 +110,9 @@ class App(customtkinter.CTk):
         self.combobox_parent_label.grid(row=1, column=0, padx=20, pady=(10, 10), sticky="nsew") 
         
         self.combobox_parent = customtkinter.CTkComboBox(self.combobox_frame,
-                                                    values=["Detector Unit", "Detector"])
+                                                    values=["Detector Unit", "Detector"],
+                                                    command=self.show_clickable
+                                                        )
         self.combobox_parent.grid(row=1, column=1, padx=20, pady=(10, 10), sticky="nsew")
         self.combobox_parent.set("- Select -")
 
@@ -101,10 +138,19 @@ class App(customtkinter.CTk):
                                                    command=self.add_button_event)
         self.add_button.grid(row=5, column=1, padx=20, pady=(10, 10))
 
-        # right sub widget: canvas containing DUs to click on
-        self.canvas = customtkinter.CTkCanvas(self.main_frame, width = 600, height=600, background='white')
-        self.canvas.grid(row=1, column=1, padx=20, pady=20, sticky="nsew")
+        # If we need to tackle a longer list of options, there is a custom dropdown frame to handle them
+        #self.longcombobox = customtkinter.CTkComboBox(self.combobox_frame)
+        #self.longcombobox.grid(row=6, column=1, padx=20, pady=10, sticky="nsew")
+        #CTkScrollableDropdownFrame(self.longcombobox, values=['abc' for i in range(1000)], justify="left", button_color="transparent", x=100)
 
+        
+        # right sub widget: canvas containing DUs to click on
+        self.canvas = customtkinter.CTkCanvas(self.main_frame, width = 500, height = 700, background='white')
+        self.canvas.grid(row=1, column=1, padx=20, pady=20, sticky="nsew")
+        self.canvas.bind('<Button-1>', self.click_canvas_event)
+        self.displayedDUtype = "None"
+
+        
         self.info_label = customtkinter.CTkLabel(self.main_frame, text=" ", font=customtkinter.CTkFont(size=16, weight="bold"))
         self.info_label.grid(row=2, column=0, padx=20, pady=20, columnspan=2)
 
@@ -126,6 +172,15 @@ class App(customtkinter.CTk):
         self.possible_parents = load_relevant_parts(p)
         self.possible_children = load_relevant_parts(c)
 
+        self.possible_parents_SNs_and_partIDs = grab_relevant_SNs_and_partIDs(self.possible_parents)
+        self.possible_parents_SNs = [entry[0] for entry in self.possible_parents_SNs_and_partIDs]
+        self.possible_parents_partIDs = [entry[1] for entry in self.possible_parents_SNs_and_partIDs]
+        self.possible_children_SNs_and_partIDs = grab_relevant_SNs_and_partIDs(self.possible_children)
+        self.possible_children_SNs = [entry[0] for entry in self.possible_children_SNs_and_partIDs]
+        self.possible_children_partIDs = [entry[1] for entry in self.possible_children_SNs_and_partIDs]
+        self.combobox_parent.configure(values=self.possible_parents_SNs)
+        self.combobox_child.configure(values=self.possible_children_SNs)
+
     def update_progressbar(self, thread):
         if thread.is_alive():
             # update progressbar
@@ -137,7 +192,8 @@ class App(customtkinter.CTk):
     # https://stackoverflow.com/a/23944658
     def segmented_button_callback(self, value):
         self.progressbar.set(0)
-        #print("segmented button clicked")
+        self.info_label.configure(text=' ')
+        self.canvas.delete("all")
         if value == "Module Loading":
             self.combobox_parent.set("- Select -")
             self.combobox_child.set("- Select -")
@@ -146,7 +202,6 @@ class App(customtkinter.CTk):
             self.position_label.configure(text="Position (derived from canvas interaction)")
             self.position_variable.set("- automatic -")
             self.position_entry.configure(state="disabled")
-            
             self.loading_wheel = threading.Thread(target=self.load_p_c, args=('Detector Unit','Module'))
             self.loading_wheel.start()
             self.update_progressbar(self.loading_wheel)
@@ -159,22 +214,90 @@ class App(customtkinter.CTk):
             self.position_label.configure(text="Position (type by hand)")
             self.position_variable.set("VxLyQz")
             self.position_entry.configure(state="normal")
-
             self.loading_wheel = threading.Thread(target=self.load_p_c, args=('Detector','Detector Unit'))
             self.loading_wheel.start()
             self.update_progressbar(self.loading_wheel)
-        
-        self.possible_parents_SNs_and_partIDs = grab_relevant_SNs_and_partIDs(self.possible_parents)
-        self.possible_parents_SNs = [entry[0] for entry in self.possible_parents_SNs_and_partIDs]
-        self.possible_parents_partIDs = [entry[1] for entry in self.possible_parents_SNs_and_partIDs]
-        self.possible_children_SNs_and_partIDs = grab_relevant_SNs_and_partIDs(self.possible_children)
-        self.possible_children_SNs = [entry[0] for entry in self.possible_children_SNs_and_partIDs]
-        self.possible_children_partIDs = [entry[1] for entry in self.possible_children_SNs_and_partIDs]
-
-        self.combobox_parent.configure(values=self.possible_parents_SNs)
-        self.combobox_child.configure(values=self.possible_children_SNs)
-
     
+    def roundedRect(self, x1, y1, width, height, radius=25, **kwargs):
+        x2 = x1 + width
+        y2 = y1 + height
+        
+        points = [x1+radius, y1,
+                  x1+radius, y1,
+                  x2-radius, y1,
+                  x2-radius, y1,
+                  x2, y1,
+                  x2, y1+radius,
+                  x2, y1+radius,
+                  x2, y2-radius,
+                  x2, y2-radius,
+                  x2, y2,
+                  x2-radius, y2,
+                  x2-radius, y2,
+                  x1+radius, y2,
+                  x1+radius, y2,
+                  x1, y2,
+                  x1, y2-radius,
+                  x1, y2-radius,
+                  x1, y1+radius,
+                  x1, y1+radius,
+                  x1, y1]
+    
+        self.canvas.create_polygon(points, **kwargs, smooth=True)
+    
+    def show_clickable(self, something):
+        parentSNIn = self.combobox_parent.get()
+        childSNIn = self.combobox_child.get()
+
+        if self.segmented_button.get() == 'Module Loading':
+            parentNameIn = 'Detector Unit'
+            childNameIn = 'Module'
+            self.canvas.delete("all")
+            for key in data.allDUs.keys():
+                if key in parentSNIn:
+                    self.displayedDUtype = key
+                    self.info_label.configure(text=' ')
+                    self.canvas.create_rectangle(40, 40, 360, 540, fill=data.fillColor_SU)
+                    for mod in data.allDUs[self.displayedDUtype]:
+                        self.roundedRect(mod['x'], mod['y'], mod['w'], mod['h'])
+                    self.canvas.create_text(140, 475, text=self.displayedDUtype, anchor='nw', font=('Arial',50), fill=data.fillColor_SU_Text)
+                    self.canvas.create_text(145, 20, text='Connector side', anchor='nw', fill=data.fillColor_SU_Text)
+                    self.canvas.create_text(145, 545, text='Capacitor side', anchor='nw', fill=data.fillColor_SU_Text)
+                    if 'FI10' in parentSNIn:
+                        self.canvas.create_text(360, 290, text='Connector side', anchor='nw', fill=data.fillColor_SU_Text, angle=90)
+                        self.canvas.create_text(20, 290, text='Capacitor side', anchor='nw', fill=data.fillColor_SU_Text, angle=90)
+                    break
+            else:
+                info_text = 'Warning: Detector Unit type could not be retrieved from Parent SN.'
+                print(f'>>> {info_text}')
+                self.info_label.configure(text=info_text)
+
+    def click_canvas_event(self, event):
+        if self.segmented_button.get() == 'Module Loading':
+            if self.displayedDUtype != 'None':
+                arrayOfModulesInDU = data.allDUs[self.displayedDUtype]
+                mouseInSomeMod = False
+                mouseX = self.canvas.canvasx(event.x)
+                mouseY = self.canvas.canvasy(event.y)
+                #mouseX, mouseX = getCursorPosition(...)
+                for slot in arrayOfModulesInDU:
+                    if isInSlot(slot, mouseX, mouseY) == True:
+                        mouseInSomeMod = True
+                        self.position_variable.set(slot['slot'])
+                        self.roundedRect(slot['x'], slot['y'], slot['w'], slot['h'], fill = data.fillColor_ActiveSlot)
+                    else:
+                        self.roundedRect(slot['x'], slot['y'], slot['w'], slot['h'], fill = data.fillColor_Slot)
+                if not mouseInSomeMod:
+                    self.position_variable.set("- automatic -")
+                    info_text = 'Warning: Place mouse in some module slot.'
+                    print(f'>>> {info_text}')
+                    self.info_label.configure(text=info_text)
+                else:
+                    self.info_label.configure(text=' ')
+        else:
+            pass
+            # ToDo: read DU connections from DB and display each connected module as a green slot
+
     def add_button_event(self):
         chi = self.combobox_child.get()
         par = self.combobox_parent.get()
@@ -192,7 +315,6 @@ class App(customtkinter.CTk):
                 'part': self.possible_children_partIDs[self.possible_children_SNs.index(chi)],
                 'part_parent': self.possible_parents_partIDs[self.possible_parents_SNs.index(par)],
             }
-            
             api.post_information('/partstreelist', part_tree)
         
 if __name__ == "__main__":
