a6a6591b0fc0e18da231d41e9a7a50d6fd0c206d
Annika Stein <annika-stein@t-online.de>
2025-08-01 10:54:18 +0200

    feat: better handling of possible cases for docker build push

--- docker-build_push_for_amd64.sh
+++ docker-build_push_for_amd64.sh
@@ -1,29 +1,39 @@
-# Find the latest tagged software version in the git repo
+# Which registry to push to. First version pushes to gitlab-registry, the second one to harbor
+#export reg="gitlab-"
+export reg=""
+
+# Find the latest tagged software version in the git repo and only proceed if this is an actual tagged version (not just the regular master branch)
 branch=`git describe --all`
 export tools_branch=${branch#*/}
-export tools_branch="1.4.3"
-echo "You are building the docker image for hgtd-tools/${tools_branch}"
 
-# Compare with eventual previously pushed docker_tag
-docker_tag_file="docker_tag.txt"
-docker_tag_change=false
-if [ -f ${docker_tag_file} ]; then
-  previous_tag=`cat ${docker_tag_file}`
-  if [[ "${previous_tag}" == *"${tools_branch}"* ]]; then
-    echo "You already use hgtd-tools docker_tag ${previous_tag}. This will not be built again, no push to registry, aborting process."
+if [ "${tools_branch}" = "master" ] ; then
+  echo "You try to build a docker image for hgtd-tools/${tools_branch}, which is not a tagged version, aborting process."
+else
+  echo "You are building the docker image for hgtd-tools/${tools_branch}"
+
+  # Compare with eventual previously pushed docker_tag
+  docker_tag_file="docker_tag.txt"
+  docker_tag_change=false
+  if [ -f ${docker_tag_file} ]; then
+    previous_tag=`cat ${docker_tag_file}`
+    if [[ "${previous_tag}" == *"${tools_branch}"* ]]; then
+      echo "You already use hgtd-tools docker_tag ${previous_tag}. This will not be built again, no push to registry, aborting process."
+    else
+      echo "The current hgtd-tools docker_tag ${tools_branch} is not the same as the last docker_tag ${previous_tag}. Proceeding with docker command."
+      docker_tag_change=true
+    fi
   else
-    echo "The current hgtd-tools docker_tag ${tools_branch} is not the same as the last docker_tag ${previous_tag}. Proceeding with docker command."
+    echo "The docker_tag.txt file is missing. Probably this is your first build / push. Proceeding with docker command."
     docker_tag_change=true
   fi
-else
-  echo "The docker_tag.txt file is missing. Probably this is your first build / push. Proceeding with docker command."
-  docker_tag_change=true
+
+  # Build and push the image for CERN registry (harbor), push requires login
+  if [ "${docker_tag_change}" = true ] ; then
+    docker buildx build --push --platform linux/amd64 -t ${reg}registry.cern.ch/hgtd/hgtd-tools:x86_64_${tools_branch} \
+    -t ${reg}registry.cern.ch/hgtd/hgtd-tools:latest .
+    if [ $? -eq 0 ]; then
+      echo "${tools_branch}" > docker_tag.txt
+    fi
+  fi
 fi
 
-# Build and push the image for CERN registry (harbor), push requires login
-if [ "${docker_tag_change}" = true ] ; then
-  docker buildx build --push --platform linux/amd64 -t registry.cern.ch/hgtd/hgtd-tools:x86_64_${tools_branch} \
-  -t registry.cern.ch/hgtd/hgtd-tools:latest .
-
-  # only if the above was successful (ToDo)
-  echo "${tools_branch}" > version.txt
